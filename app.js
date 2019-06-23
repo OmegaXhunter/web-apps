4a9bb0abec92e140c2cc6b649f833ba91d97cd9c
Damian Diaz <damiandiaze@gmail.com>
2019-06-22 23:25:17 -0300

    agregado mas cosas
    distancia ,distancia en 3d

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 let app = angular.module('OmegaXApp', []);
 
     app.controller('MainController', function ($http,$scope) {
+        //distancia basica
+        $scope.pos1=0;
+        $scope.pos2=0;
+        $scope.resultado=0;
+        $scope.calcularResultado=function(){
+            
+            $scope.resultado=Math.abs($scope.pos2-$scope.pos1);
+        }
+        //distancia en 2 dimensiones
         $scope.posx1=0;
         $scope.posx2=0;
         $scope.posy1=0;
@@ -8,14 +17,17 @@ let app = angular.module('OmegaXApp', []);
         $scope.distancia2DResultado=0;
 
         $scope.calcularResultado2d=function(){
-            //console.log($scope.posx1)
-            //console.log($scope.posx2)
-            //console.log($scope.posy1)
-            //console.log($scope.posy2)
-
-            //console.log( Math.sqrt(Math.pow($scope.posx2-$scope.posx1,2)+Math.pow($scope.posy2-$scope.posy1,2))
-            //)
-            $scope.distancia2DResultado= Math.sqrt(Math.pow($scope.posx2-$scope.posx1,2)+Math.pow($scope.posy2-$scope.posy1,2));
+          $scope.distancia2DResultado= Math.sqrt(Math.pow($scope.posx2-$scope.posx1,2)+Math.pow($scope.posy2-$scope.posy1,2));
         }
-
+        //distancia en 3 dimensiones
+        $scope.pos3dx1=0;
+        $scope.pos3dy1=0;
+        $scope.pos3dz1=0;
+        $scope.pos3dx2=0;
+        $scope.pos3dy2=0;
+        $scope.pos3dz2=0;
+        $scope.distancia3DResultado=0;
+        $scope.calcularResultado3D=function(){
+          $scope.distancia3DResultado=Math.sqrt(Math.pow($scope.pos3dx2-$scope.pos3dx1,2)+Math.pow($scope.pos3dy2-$scope.pos3dy1,2)+Math.pow($scope.pos3dz2-$scope.pos3dz1,2));
+        }
     })
